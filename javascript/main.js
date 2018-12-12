65a0ecf136cd18815472ec88fb39198ee17048c5
Jw <jwhaeke@live.nl>
2018-12-12 12:22:21 +0100

    moving to JSON like object creation

--- javascript/main.js
+++ javascript/main.js
@@ -107,3 +107,23 @@
 // Magazine.prototype.constructor = Magazine;
 
 // console.log(mag1);
+
+//Object Of Protos
+
+const bookProtos = {
+    getSummary: function() {
+        return `${this.title} was written by ${this.author} in ${this.year}`;
+    },
+    getAge: function(){
+        const years = new Date().getFullYear() - this.year;
+        return `${this.title} is ${years} years old`;
+    }
+}
+
+// Create Object
+const book1 = Object.create(bookProtos);
+book1.title = 'Book One';
+book1.title = 'John Doe';
+book1.year = '2013';
+
+console.log(book1);
