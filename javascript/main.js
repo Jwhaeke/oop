3ed2f6034032f3a2d59fdc6f0051e96b4ba187ee
Jw <jwhaeke@live.nl>
2018-12-12 13:18:22 +0100

    Finished ES6 object creating and moving to sub-classes

--- javascript/main.js
+++ javascript/main.js
@@ -134,6 +134,38 @@
 
 // console.log(book1);
 
+// class Book {
+//     constructor(title, author, year) {
+//         this.title = title;
+//         this.author = author;
+//         this.year = year;
+//     }
+//     getSummary() {
+//         return `${this.title} was written by ${this.author} in ${this.year}`;
+//     }
+//     getAge() {
+//     const years = new Date().getFullYear() - this.year;
+//     return `${this.title} is ${years} years old`;
+//     }
+//     revise(newYear) {
+//         this.year = newYear;
+//         this.revised = true;
+//     }
+//     static topBookStore(){
+//         return 'Ako';
+//     }
+// }
+
+// //Instantiate Object
+// const book1 = new Book('Book one', 'John Doe', '2013');
+
+// console.log(book1);
+// book1.revise('2018');
+// console.log(book1);
+
+// //Calling a function without initialising an object
+// console.log(Book.topBookStore());
+
 class Book {
     constructor(title, author, year) {
         this.title = title;
@@ -143,20 +175,4 @@ class Book {
     getSummary() {
         return `${this.title} was written by ${this.author} in ${this.year}`;
     }
-    getAge() {
-    const years = new Date().getFullYear() - this.year;
-    return `${this.title} is ${years} years old`;
-    }
-    revise(newYear) {
-        this.year = newYear;
-        this.revised = true;
-    }
 }
-
-//Instantiate Object
-const book1 = new Book('Book one', 'John Doe', '2013');
-
-console.log(book1);
-book1.revise('2018');
-console.log(book1);
-
