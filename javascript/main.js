e035b2aa9d9f3b3d14a017a26f52482f09cad861
Jw <jwhaeke@live.nl>
2018-12-12 12:13:47 +0100

    starting object create

--- javascript/main.js
+++ javascript/main.js
@@ -1,47 +1,82 @@
 
-// // Object literal
-// // const book1 = {
-// //     title: 'Book One',
-// //     author: 'John Doe',
-// //     year: '2013',
-// //     getSummary: function() {
-// //         return `${this.title} was written by ${this.author} in ${this.year}`;
-// //     }
-// // };
+// // // Object literal
+// // // const book1 = {
+// // //     title: 'Book One',
+// // //     author: 'John Doe',
+// // //     year: '2013',
+// // //     getSummary: function() {
+// // //         return `${this.title} was written by ${this.author} in ${this.year}`;
+// // //     }
+// // // };
 
-// // document.write(book1.getSummary());
+// // // document.write(book1.getSummary());
 
-// // const book2 = {
-// //     title: 'Book Two',
-// //     author: 'Jane Doe',
-// //     year: '2016',
-// //     getSummary: function() {
-// //         return `${this.title} was written by ${this.author} in ${this.year}`;
-// //     }
-// // };
+// // // const book2 = {
+// // //     title: 'Book Two',
+// // //     author: 'Jane Doe',
+// // //     year: '2016',
+// // //     getSummary: function() {
+// // //         return `${this.title} was written by ${this.author} in ${this.year}`;
+// // //     }
+// // // };
 
-// // document.write(book2.getSummary());
+// // // document.write(book2.getSummary());
 
-// // console.log(Object.values(book2));
-// // console.log(Object.keys(book2));
+// // // console.log(Object.values(book2));
+// // // console.log(Object.keys(book2));
 
 
-// //Constructor
+// // //Constructor
+
+// // // function Book(title, author, year) {
+// // //     this.title = title;
+// // //     this.author = author;
+// // //     this.year = year;
+// // //     this.getSummary = function () {
+// // //         return `${this.title} was written by ${this.author} in ${this.year}`;
+// // //     };
+    
+// // // }
+
+// // // const book1 = new Book('Book One', 'John Doe', '2013');
+// // // const book2 = new Book('Book Two', 'Jane Doe', '2016');
+
+// // // console.log(book1.getSummary());
 
 // // function Book(title, author, year) {
 // //     this.title = title;
 // //     this.author = author;
 // //     this.year = year;
-// //     this.getSummary = function () {
-// //         return `${this.title} was written by ${this.author} in ${this.year}`;
-// //     };
-    
+// // }
+
+// // //getSummary
+
+// // Book.prototype.getSummary = function() {
+// //     return `${this.title} was written by ${this.author} in ${this.year}`;
+// // };
+
+// // //getAge
+
+// // Book.prototype.getAge = function() {
+// //     const years = new Date().getFullYear() - this.year;
+// //     return `${this.title} is ${years} years old`;
+// // };
+
+// // // Revise / Change Year
+// // Book.prototype.revise = function(newYear) {
+// //     this.year = newYear;
+// //     this.revised = true;
 // // }
 
 // // const book1 = new Book('Book One', 'John Doe', '2013');
 // // const book2 = new Book('Book Two', 'Jane Doe', '2016');
 
-// // console.log(book1.getSummary());
+// // console.log(book2);
+// // book2.revise('2018');
+// // console.log(book2);
+
+
+// // Constructor
 
 // function Book(title, author, year) {
 //     this.title = title;
@@ -55,55 +90,20 @@
 //     return `${this.title} was written by ${this.author} in ${this.year}`;
 // };
 
-// //getAge
+// // Magazine Constructor
+// function Magazine(title, author, year, month) {
+//     Book.call(this, title,author, year);
 
-// Book.prototype.getAge = function() {
-//     const years = new Date().getFullYear() - this.year;
-//     return `${this.title} is ${years} years old`;
-// };
-
-// // Revise / Change Year
-// Book.prototype.revise = function(newYear) {
-//     this.year = newYear;
-//     this.revised = true;
+//     this.month = month;
 // }
 
-// const book1 = new Book('Book One', 'John Doe', '2013');
-// const book2 = new Book('Book Two', 'Jane Doe', '2016');
+// //Inherit Prototype
+// Magazine.prototype = Object.create(Book.prototype);
 
-// console.log(book2);
-// book2.revise('2018');
-// console.log(book2);
+// // Instantiate Magazine
+// const mag1 = new Magazine('Mag One', 'John Doe', '2018', 'Jan');
 
+// // Use Magazine Constructor
+// Magazine.prototype.constructor = Magazine;
 
-// Constructor
-
-function Book(title, author, year) {
-    this.title = title;
-    this.author = author;
-    this.year = year;
-}
-
-//getSummary
-
-Book.prototype.getSummary = function() {
-    return `${this.title} was written by ${this.author} in ${this.year}`;
-};
-
-// Magazine Constructor
-function Magazine(title, author, year, month) {
-    Book.call(this, title,author, year);
-
-    this.month = month;
-}
-
-//Inherit Prototype
-Magazine.prototype = Object.create(Book.prototype);
-
-// Instantiate Magazine
-const mag1 = new Magazine('Mag One', 'John Doe', '2018', 'Jan');
-
-// Use Magazine Constructor
-Magazine.prototype.constructor = Magazine;
-
-console.log(mag1);
+// console.log(mag1);
