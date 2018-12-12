0c84243115b6d31b2b97680d290ebdadeab0eb6b
Jw <jwhaeke@live.nl>
2018-12-12 11:32:43 +0100

    working on constructors

--- javascript/main.js
+++ javascript/main.js
@@ -24,3 +24,51 @@
 
 // console.log(Object.values(book2));
 // console.log(Object.keys(book2));
+
+
+//Constructor
+
+// function Book(title, author, year) {
+//     this.title = title;
+//     this.author = author;
+//     this.year = year;
+//     this.getSummary = function () {
+//         return `${this.title} was written by ${this.author} in ${this.year}`;
+//     };
+    
+// }
+
+// const book1 = new Book('Book One', 'John Doe', '2013');
+// const book2 = new Book('Book Two', 'Jane Doe', '2016');
+
+// console.log(book1.getSummary());
+
+function Book(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+}
+
+//getSummary
+
+Book.prototype.getSummary = function() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+};
+
+//getAge
+
+Book.prototype.getAge = function() {
+    const years = new Date().getFullYear() - this.year;
+    return `${this.title} is ${years} years old`;
+};
+
+// Revise / Change Year
+Book.prototype.revise = function(newYear) {
+    this.year = newYear;
+    this.revised = true;
+}
+
+const book1 = new Book('Book One', 'John Doe', '2013');
+const book2 = new Book('Book Two', 'Jane Doe', '2016');
+
+console.log(book2.getAge());
