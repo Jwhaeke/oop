269950adb716a8940e5eb07be507739a600a5a92
Jw <jwhaeke@live.nl>
2018-12-12 11:04:43 +0100

    commented out and moving to oop

--- javascript/main.js
+++ javascript/main.js
@@ -1,23 +1,26 @@
 
 // Object literal
-const book1 = {
-    title: 'Book One',
-    author: 'John Doe',
-    year: '2013',
-    getSummary: function() {
-        return `${this.title} was written by ${this.author} in ${this.year}`;
-    }
-};
+// const book1 = {
+//     title: 'Book One',
+//     author: 'John Doe',
+//     year: '2013',
+//     getSummary: function() {
+//         return `${this.title} was written by ${this.author} in ${this.year}`;
+//     }
+// };
 
 // document.write(book1.getSummary());
 
-const book2 = {
-    title: 'Book Two',
-    author: 'Jane Doe',
-    year: '2016',
-    getSummary: function() {
-        return `${this.title} was written by ${this.author} in ${this.year}`;
-    }
-};
+// const book2 = {
+//     title: 'Book Two',
+//     author: 'Jane Doe',
+//     year: '2016',
+//     getSummary: function() {
+//         return `${this.title} was written by ${this.author} in ${this.year}`;
+//     }
+// };
 
-document.write(book2.getSummary());
+// document.write(book2.getSummary());
+
+// console.log(Object.values(book2));
+// console.log(Object.keys(book2));
